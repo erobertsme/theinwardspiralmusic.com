3e4f572f62a46f4e514d6c88a87e1de50f44ce66
omfgtora <spaminol@gmail.com>
2020-02-27 20:51:21 -0800

    Update image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import HeaderImage from '../assets/images/JULYEP_xsm.png'
+import HeaderImage from '../assets/images/artist_portrait.png'
 
 const socials = [
   { name:'facebook-f', url:'https://facebook.com/theinwardspiral' },
